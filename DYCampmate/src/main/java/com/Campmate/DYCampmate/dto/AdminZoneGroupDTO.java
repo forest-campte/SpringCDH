87fe0cd69f3f7e9b1d760f2e8bf49c9f363a2fe1
cdh <chdh23@gmail.com>
2025-10-30 20:29:49 +0900

    test commit

--- DYCampmate/src/main/java/com/Campmate/DYCampmate/dto/AdminZoneGroupDTO.java
+++ DYCampmate/src/main/java/com/Campmate/DYCampmate/dto/AdminZoneGroupDTO.java
@@ -8,6 +8,9 @@ import java.util.List;
 public record AdminZoneGroupDTO(
         String name, // 1. admins 테이블의 name
         List<ZoneHomeViewDTO> sites // 2. 해당 admin에 속한 CampingZone 리스트
+                                    // admin 캠핑장
+                                    // ex ) name, address, campingStyle,campingType, imageUrl,
+
 ) {
 
 
